579742a808060ed1c57f4acf46b5aeb601dff5fc
Lidiya <lidakonstantinova@mail.ru>
2023-01-03 14:53:43 +0400

    модалки
    продолжается работа над модальными окнами

--- source/js/index.js
+++ source/js/index.js
@@ -1,9 +1,27 @@
-var openMenu = document.querySelector(".header-mob-js");
-var openMenuBtn = document.querySelector(".heading-section__menu-btn");
-var closeMenu = document.querySelector(".header-mob__close-btn");
-var signInModal = document.querySelector(".sign-in-modal-js");
-var openSignInModal = document.querySelector(".sign-in-modal-btn-js");
-var closeSignInModal = document.querySelector(".sign-in-modal__close-btn")
+let openMenu = document.querySelector(".header-mob-js");
+let openMenuBtn = document.querySelector(".heading-section__menu-btn");
+let closeMenu = document.querySelector(".header-mob__close-btn");
+let signInModal = document.querySelector(".sign-in-modal-js");
+let openSignInModal = document.querySelector(".sign-in-modal-btn-js");
+let closeSignInModal = document.querySelectorAll(".modal__close-btn")[0];
+let registerModal = document.querySelector(".register-modal-js");
+let openRegisterModal = document.querySelector(".register_modal-btn-js");
+let closeRegisterModal = document.querySelectorAll(".modal__close-btn")[1];
+let checkbox = document.querySelector(".cb-js");
+let formBtn = document.querySelector(".btn-register-js");
+let checkboxLabel = document.querySelector(".cb-label-js");
+
+
+const openModal = function () {
+    signInModal.classList.remove("hidden");
+    signInModal.classList.add("modal");
+}
+
+const closeModal = function () {
+    signInModal.classList.remove("modal");
+    signInModal.classList.add("hidden");
+}
+
 
 openMenuBtn.addEventListener("click", function() {
     openMenu.classList.add("header-mob");
@@ -13,13 +31,33 @@ closeMenu.addEventListener("click", function() {
     openMenu.classList.remove("header-mob");
 })
 
-openSignInModal.addEventListener("click", function() {
-    signInModal.classList.remove("hidden");
-    signInModal.classList.add("sign-in-modal");
+window.addEventListener("keydown", function(e) {
+    if (e.key === "Escape") {
+        signInModal.classList.remove("modal");
+        signInModal.classList.add("hidden");
+        registerModal.classList.remove("modal");
+        registerModal.classList.add("hidden");
+    }
 })
 
+openSignInModal.addEventListener("click", openModal);
+closeSignInModal.addEventListener("click", closeModal);
 
-closeSignInModal.addEventListener("click", function() {
-    signInModal.classList.remove("sign-in-modal");
-    signInModal.classList.add("hidden");
+openRegisterModal.addEventListener("click", function() {
+    registerModal.classList.remove("hidden");
+    registerModal.classList.add("modal");
+})
+
+closeRegisterModal.addEventListener("click", function() {
+    registerModal.classList.remove("modal");
+    registerModal.classList.add("hidden");
+})
+
+checkbox.addEventListener("click", function() {
+    if (checkbox.checked) {
+        formBtn.removeAttribute("disabled");
+        checkboxLabel.classList.remove("modal__form__checkbox-wrap__label-disabled");
+    } else {
+        formBtn.setAttribute("diasbled");
+    }
 })
