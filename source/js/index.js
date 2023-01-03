a1c6c793d021f5cdc5dd17ed13aa57844a27d3cb
Lidiya <lidakonstantinova@mail.ru>
2023-01-03 17:32:34 +0400

    модалки
    сверстаны все модальные окна + написаны черновые скрипты на открытие/закрытие/чекбоксы

--- source/js/index.js
+++ source/js/index.js
@@ -7,9 +7,15 @@ let closeSignInModal = document.querySelectorAll(".modal__close-btn")[0];
 let registerModal = document.querySelector(".register-modal-js");
 let openRegisterModal = document.querySelector(".register_modal-btn-js");
 let closeRegisterModal = document.querySelectorAll(".modal__close-btn")[1];
-let checkbox = document.querySelector(".cb-js");
-let formBtn = document.querySelector(".btn-register-js");
-let checkboxLabel = document.querySelector(".cb-label-js");
+let checkbox = document.querySelectorAll(".cb-js")[0];
+let formBtn = document.querySelectorAll(".btn-register-js")[0];
+let checkboxLabel = document.querySelectorAll(".cb-label-js")[0];
+let openMessageModal = document.querySelector(".msg-btn-js");
+let messageModal = document.querySelector(".message-modal-js");
+let closeMessageModal = document.querySelector(".modal__close-btn--msg");
+let checkboxMessage = document.querySelectorAll(".cb-js")[1];
+let formBtnMessage = document.querySelectorAll(".btn-register-js")[1];
+let checkboxLabelMessage = document.querySelectorAll(".cb-label-js")[1];
 
 
 const openModal = function () {
@@ -37,6 +43,8 @@ window.addEventListener("keydown", function(e) {
         signInModal.classList.add("hidden");
         registerModal.classList.remove("modal");
         registerModal.classList.add("hidden");
+        messageModal.classList.remove("modal");
+        messageModal.classList.add("hidden");
     }
 })
 
@@ -60,4 +68,23 @@ checkbox.addEventListener("click", function() {
     } else {
         formBtn.setAttribute("diasbled");
     }
+})
+
+checkboxMessage.addEventListener("click", function() {
+    if (checkboxMessage.checked) {
+        formBtnMessage.removeAttribute("disabled");
+        checkboxLabelMessage.classList.remove("modal__form__checkbox-wrap__label-disabled");
+    } else {
+        formBtnMessage.setAttribute("diasbled");
+    }
+})
+
+openMessageModal.addEventListener("click", function() {
+    messageModal.classList.remove("hidden");
+    messageModal.classList.add("modal");
+})
+
+closeMessageModal.addEventListener("click", function() {
+    messageModal.classList.remove("modal");
+    messageModal.classList.add("hidden");
 })
